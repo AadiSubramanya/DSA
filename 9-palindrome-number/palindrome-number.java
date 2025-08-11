af363a2a152d0ba33b7b326eee5909108cd46544
Aadi Subramanya <aadisubramanya@gmail.com>
2025-08-11 09:45:53 +0530

    Time: 4 ms (100.00%) | Memory: 44.5 MB (10.98%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
         
-        int original = x;
+        int og = x;
         int rev = 0;
         
         while (x>0) {
@@ -10,6 +10,6 @@ class Solution {
             x /= 10;
         }
         
-        return rev == original;
+        return rev == og;
     }
 }
