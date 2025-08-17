963ba7027df838ce13bbbcd23cc363f1c0183abf
Aadi Subramanya <aadisubramanya@gmail.com>
2025-08-17 21:08:38 +0530

    Time: 3 ms (55.00%) | Memory: 42.3 MB (78.41%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -3,15 +3,14 @@ class Solution {
     if (num1.equals("0") || num2.equals("0")) return "0";
 
     int m = num1.length(), n = num2.length();
-    int[] result = new int[m + n];
+    int[] result = new int[m+n];
 
-    for (int i = m - 1; i >= 0; i--) {
-        for (int j = n - 1; j >= 0; j--) {
-            int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-            int sum = mul + result[i + j + 1];
-
-            result[i + j + 1] = sum % 10;
-            result[i + j] += sum / 10;
+    for (int i = m-1; i>=0; i--) {
+        for (int j = n-1; j>=0; j--) {
+            int mul = (num1.charAt(i)-'0') * (num2.charAt(j)-'0');
+            int sum = mul+result[i+j+1];
+            result[i+j+1] = sum%10;
+            result[i+j] += sum/10;
         }
     }
 
