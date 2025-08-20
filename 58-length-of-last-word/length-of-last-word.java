027ad7c7e6938a46711217609794a2f08ff38e82
Aadi Subramanya <aadisubramanya@gmail.com>
2025-08-20 21:47:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (65.75%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,14 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int c = 0, flag = 0;
+        int c = 0;
         for (int i = s.length()-1; i>=0; i--){
             if (s.charAt(i) != ' '){
                 c++;
-                if (flag != 1){
-                    flag = 1;
-                }
             }
-            if (s.charAt(i) == ' ' && flag == 1){
+            if (s.charAt(i) == ' ' && c > 0){
                 break;
             }
         }
