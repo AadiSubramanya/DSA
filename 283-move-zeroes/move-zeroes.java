7a77c1047267742abb8a86675e3cce7ccb36f29b
Aadi Subramanya <aadisubramanya@gmail.com>
2025-08-23 23:54:37 +0530

    Time: 3 ms (21.25%) | Memory: 46.1 MB (68.23%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,24 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int backshots = nums.length-1, temp;
-        while (nums[backshots] == 0){
-            if (backshots == 0){
-                return;
-            }
-            else{
-                backshots--;
-            }
-        }
-        for (int i = backshots-1; i>=0; i--){
-            if (nums[i] == 0){
-                int k = i;
-                while (k < backshots){
-                    temp = nums[k+1];
-                    nums[k+1] = nums[k];
-                    nums[k] = temp;
-                    k++;
+        if (nums.length == 1) return;
+
+        int backshots = 0; // position to place the next non-zero
+        int temp;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                // swap nums[i] with nums[backshots] if needed
+                if (i != backshots) {
+                    temp = nums[i];
+                    nums[i] = nums[backshots];
+                    nums[backshots] = temp;
                 }
-                backshots--;
+                backshots++;
             }
         }
     }
