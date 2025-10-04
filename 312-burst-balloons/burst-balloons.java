fda152c7d693c104f16441fdd116546c22ee2ecd
Aadi Subramanya <aadisubramanya@gmail.com>
2025-10-04 18:45:05 +0530

    Time: 36 ms (81.09%) | Memory: 42.9 MB (41.81%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[n + 1] = 1;
+        for (int i = 0; i < n; i++) arr[i + 1] = nums[i];
+
+        int[][] dp = new int[n + 2][n + 2];
+
+        // length is the interval size
+        for (int len = 2; len < n + 2; len++) {
+            for (int left = 0; left < n + 2 - len; left++) {
+                int right = left + len;
+                for (int i = left + 1; i < right; i++) {
+                    dp[left][right] = Math.max(dp[left][right],
+                        dp[left][i] + dp[i][right] + arr[left] * arr[i] * arr[right]);
+                }
+            }
+        }
+
+        return dp[0][n + 1];
+    }
+}
