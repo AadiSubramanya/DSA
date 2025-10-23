eaa874ecf62df88349d128a7ba0f57489c48d368
Aadi Subramanya <aadisubramanya@gmail.com>
2025-10-23 21:31:17 +0530

    Time: 3 ms (73.02%) | Memory: 15.9 MB (35.03%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.c
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.c
@@ -11,7 +11,7 @@ struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
     int index=numsSize/2;
     struct TreeNode *left=sortedArrayToBST(nums,(numsSize/2));
     struct TreeNode *right=sortedArrayToBST(nums+(numsSize/2)+1,numsSize-index-1);
-    struct TreeNode *newnode=(struct TreeNode *)malloc(sizeof(struct TreeNode));
+    struct TreeNode * newnode=(struct TreeNode *)malloc(sizeof(struct TreeNode));
     newnode->val=nums[index];
     newnode->left=left;
     newnode->right=right;
